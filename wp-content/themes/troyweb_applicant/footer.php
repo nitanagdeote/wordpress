15cc0d0ca65d2fd3499d68b9af728fca919a9e5c
nitanagdeote <nitanagdeote1@gmail.com>
2024-09-09 08:28:21 -0400

    formatted-code

--- wp-content/themes/troyweb_applicant/footer.php
+++ wp-content/themes/troyweb_applicant/footer.php
@@ -1,33 +1,21 @@
-
 <?php wp_footer(); ?>
-<div class='footer-top'>
-  
-</div>
-<footer >
-
-<div class='container'>
-         <!-- Add the logo on the left -->
- <div class="nav-logo-bottom">
-        <a href="<?php echo home_url(); ?>">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tw-logo-footer.png" alt="Site Logo">
-        </a>
-       
-   
-    
-
-        
-</div>
-<div>     
-    <i class="fa-brands fa-facebook"></i>
-    <i class="fa-brands fa-instagram"></i>
-    <i class="fa-brands fa-x-twitter"></i>
-    <i class="fa-brands fa-linkedin"></i>
-</div>
+<div class='footer-top'></div>
+<footer>
+    <div class='container'>
+        <!-- Add the logo on the left -->
+        <div class="nav-logo-bottom">
+            <a href="<?php echo home_url(); ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tw-logo-footer.png" alt="Site Logo">
+            </a>
+        </div>
+        <div>
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-x-twitter"></i>
+            <i class="fa-brands fa-linkedin"></i>
+        </div>
     </div>
-
-
-</div>
-  
 </footer>
 </body>
+
 </html>
--- wp-content/themes/troyweb_applicant/header.php
+++ wp-content/themes/troyweb_applicant/header.php
@@ -1,35 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TroyWebApplicant</title>
     <?php wp_head(); ?>
 </head>
+
 <body>
-<header>
+    <header>
 
 
 
-    <div class='container'>
-         <!-- Add the logo on the left -->
- <div class="nav-logo">
-        <a href="<?php echo home_url(); ?>">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tw-logo-header.png" alt="Site Logo">
-        </a>
-    </div>
-<?php 
-wp_nav_menu(
-array(
-'theme_location'=> 'top-menu',
-'menu_class'=>'top-bar'
-)
-
-);
-
-?>
- <a href="http://localhost/wordpress-6.6.1/wordpress/?page_id=88" >
-        <button class='nav-button'>Contact Me</button>
-    </a>
-</div>
-</header>
+        <div class='container'>
+            <!-- Add the logo on the left -->
+            <div class="nav-logo">
+                <a href="<?php echo home_url(); ?>">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/tw-logo-header.png"
+                        alt="Site Logo">
+                </a>
+            </div>
+            <?php
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'top-menu',
+                    'menu_class' => 'top-bar'
+                )
+            );
+            ?>
+            <a href="http://localhost/wordpress-6.6.1/wordpress/?page_id=88">
+                <button class='nav-button'>Contact Me</button>
+            </a>
+        </div>
+    </header>
