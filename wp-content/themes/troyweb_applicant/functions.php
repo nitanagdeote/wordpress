a2102c946eb4c179eb8c4fae8a08a50dd3c48a7b
nitanagdeote <nitanagdeote1@gmail.com>
2024-09-09 00:54:08 -0400

    added-pages

---
+++ wp-content/themes/troyweb_applicant/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+//load css
+function load_css() {
+    // Register the Bootstrap CSS file
+    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
+    
+    // Enqueue the registered Bootstrap CSS file
+    wp_enqueue_style('bootstrap');
+}
+
+// Hook the 'load_css' function to 'wp_enqueue_scripts', which is the correct action for enqueuing styles and scripts
+add_action('wp_enqueue_scripts', 'load_css');
+
+
+// Load JS
+
+function load_js() {
+    wp_enqueue_script('jquery');
+    // Register the Bootstrap JS file
+    wp_register_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery', false, true);
+    
+    // Enqueue the registered Bootstrap JS file
+    wp_enqueue_script('bootstrap');
+}
+
+// Hook the 'load_js' function to 'wp_enqueue_scripts', which is the correct action for enqueuing styles and scripts
+add_action('wp_enqueue_scripts', 'load_js');
